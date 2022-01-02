6042f244d3acbd446704d95945ab7edd91d474d5
Daniel Ik <codingeinstein@gmail.com>
2022-01-02 19:42:54 +0100

    cbow model

--- src/model.py
+++ src/model.py
@@ -1,6 +1,22 @@
 import torch
 import torch.nn as nn
 
+class CBOW(nn.Module):
+    def __init__(self,input_size,hidden_size):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size,hidden_size)
+        self.fc2 = nn.Linear(hidden_size,input_size)
+
+    def forward(self,x):
+        #window,target,window
+        #x of shape (window+window,bs,input_size)
+        x = sum([*x]) #(bs,input_size)
+        x = self.fc1(x)
+        x = self.fc2(x)
+
+        return x
+
+
 class Net(nn.Module):
     def __init__(self,input_size,hidden_size=128,num_layers=1,drop=0):
         super().__init__()
