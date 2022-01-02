ce0a463658546fa64b62bb90fe95fcb29e06b5c6
Daniel Ik <codingeinstein@gmail.com>
2022-01-02 19:43:14 +0100

    generate cbow data

--- src/process_data.py
+++ src/process_data.py
@@ -72,16 +72,45 @@ class Proc:
         pickle.dump(X,open('../data/X.pkl','wb'))
         pickle.dump(Y,open('../data/Y.pkl','wb'))
 
+
+    def save_cbow_data(self,window):
+        
+        X = []
+        Y = []
+
+        for idx in self.json:
+            words = self.json[idx].split()
+            words = [word if word in self.vocab else 'UNK' for word in words]
+            if len(words) > ((2*window)+1):
+                for n in range(len(words)-(2*window)+1):
+
+                    full = words[n:(2*window)+1+n]
+
+                    front = full[:window]
+                    back = full[-window:]
+                    x = front+back
+                    y = full[window]
+
+                    X.append(x)
+                    Y.append(y)
+
+                
+                pickle.dump(X,open('../data/cbowX.pkl','wb'))
+                pickle.dump(Y,open('../data/cbowY.pkl','wb'))
+
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--past_count',help='Number of previous words as context')
 parser.add_argument('--freq_thresh',help='How many times a token has to appear in the dataset')
+parser.add_argument('--window',help='window for CBOW dataset')
 
 args = parser.parse_args()
 
 past_count = int(args.past_count or 3)
 freq_thresh = int(args.freq_thresh or 5)
-
+window = int(args.window or 1)
 
 data_proc = Proc(freq_thresh=freq_thresh,past_count=past_count)
 data_proc.save_data()
+data_proc.save_cbow_data(window)
