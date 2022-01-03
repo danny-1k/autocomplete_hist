96ffc374aa48935cd661a6e1982bc133ed15175e
Daniel Ik <codingeinstein@gmail.com>
2022-01-03 13:38:24 +0100

    updated model.py

--- src/model.py
+++ src/model.py
@@ -17,14 +17,20 @@ class CBOW(nn.Module):
     def save_model(self):
         torch.save(self.state_dict(),'checkpoints/cbow_model.pt')
 
-    def load_model(self,f):
+    def load_model(self):
         self.load_state_dict(torch.load('checkpoints/cbow_model.pt',map_location='cpu'))
 
 
 class Net(nn.Module):
-    def __init__(self,input_size,hidden_size=128,num_layers=1,drop=0):
+    def __init__(self,input_size,embedding,hidden_size=128,num_layers=1,drop=0):
         super().__init__()
-        self.lstm = nn.LSTM(input_size=input_size,
+        self.embedding = embedding
+        self.embedding.requires_grad_(False)
+        self.embed_out_size = self.embedding.shape[0] 
+
+        assert input_size==embedding.shape[1],f'Input size ({input_size}) should match input size of embedding {embedding.shape[1]}'
+
+        self.lstm = nn.LSTM(input_size=self.embed_out_size,
                     hidden_size=hidden_size,num_layers=num_layers,
                     dropout=drop if num_layers>1 else 0,
                     batch_first=True)
@@ -33,13 +39,24 @@ class Net(nn.Module):
 
 
     def forward(self,x,hidden=None):
+        #x of shape (bs,timesteps,input_size)
+        x = self.embed_encode(x)
         x,hidden = self.lstm(x)
         x = self.drop(x[:,-1,])
         x = self.fc(x)
         return x,hidden
 
+    
+    def embed_encode(self,x):
+        out = torch.zeros((x.shape[0],x.shape[1],self.embed_out_size))
+
+        for t in range(x.shape[1]):
+            out[:,t] = (self.embedding@x[:,t].T).T
+
+        return out
+
     def save_model(self):
         torch.save(self.state_dict(),'checkpoints/classifier_model.pt')
 
-    def load_model(self,f):
-        self.load_state_dict(torch.load('checkpoints/cbow_model.pt',map_location='cpu'))
+    def load_model(self):
+        self.load_state_dict(torch.load('checkpoints/classifier_model.pt',map_location='cpu'))
